754eda67160259d1c5d476c5e2bbc29c83c616c9
wam-databricks <40581773+wam-databricks@users.noreply.github.com>
2018-06-25 15:19:31 -0700

    Change the parameters

--- notebooks/examples/NotebookCaller.scala
+++ notebooks/examples/NotebookCaller.scala
@@ -1,11 +1,11 @@
 // Databricks notebook source
-dbutils.notebook.run("./NotebookParam", 30, Map("foo" -> "myScalaValue"))
+dbutils.notebook.run("./NotebookParam", 30, Map("foo" -> "Scala Code"))
 
 // COMMAND ----------
 
 // MAGIC %py
-// MAGIC dbutils.notebook.run("NotebookParam", 60, {"foo": "MyPyValue"})
+// MAGIC dbutils.notebook.run("NotebookParam", 60, {"foo": "Python Code"})
 
 // COMMAND ----------
 
-// MAGIC %run ./NotebookParam $foo="MyRunValue"
+// MAGIC %run ./NotebookParam $foo="Databricks Run"
